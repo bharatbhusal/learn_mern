d15b9a62f3f0159bff6caa289ebf00c3605e3956
Bharat Bhusal <bharat@Bharats-MacBook-Air.local>
2023-06-29 00:41:28 +0530

    logs and errorHandler done

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.bharatbhusal.com',
+    'https:bharatbhusal.com',
+    'https://www.google.com/'
+]
+module.exports = allowedOrigins
