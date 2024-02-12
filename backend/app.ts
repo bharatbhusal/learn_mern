6c58c13dafb49196a2ad0653b061814157e42a98
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-02-12 16:26:30 +0530

    src

--- backend/app.ts
+++ backend/app.ts
@@ -10,7 +10,7 @@ app.use(morgan("dev"))
 
 app.use(express.json());
 
-app.use("/api/notes", notesRoutes)
+app.use("/notes", notesRoutes)
 
 app.use((req, res, next) => {
     next(createHttpError(404, "Endpoint not found"));
